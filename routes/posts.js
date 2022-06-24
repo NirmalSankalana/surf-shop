b1c8cd68dda417242757d2bb44f27b726a1de7bd
nirmalsankalana71@gmail.com <nirmalsankalana71@gmail.com>
2022-06-24 16:20:03 +0530

    Add routes to posts

---
+++ routes/posts.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET post index /posts */
+router.get('/', (req, res, next) =>{
+    res.send('/posts')
+  });
+
+/* GET post new /posts/new */
+router.get('/new', (req, res, next) =>{
+    res.send('/posts/new')
+  });
+
+/* POST post create /posts */
+router.post('/', (req, res, next) =>{
+    res.send('CREATE /posts')
+  });
+
+/* GET post show. /posts/:id */
+router.get('/:id', (req, res, next) =>{
+    res.send('SHOW /posts/:id')
+  });
+
+/* GET post edit. /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) =>{
+    res.send('EDIT /posts/:id/edit')
+  });
+
+/* PUT post update. /posts/:id */
+router.put('/:id', (req, res, next) =>{
+    res.send('UPDATE /posts/:id')
+  });
+
+/* DELETE post destroy. /posts/:id */
+router.delete('/:id', (req, res, next) =>{
+    res.send('DELETE /posts/:id')
+  });
+  
+  
+  module.exports = router;
